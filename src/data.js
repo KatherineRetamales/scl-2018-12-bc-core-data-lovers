a59c4e9101f6abcfe44f745faf314519e5772b1b
katherine <kretamales280@gmail.com>
2019-01-21 04:10:41 -0300

    Se mejoro el diseño query

--- src/data.js
+++ src/data.js
@@ -1,44 +1,27 @@
 const data = Object.values(window.LOL.data);
-const dataImg = (data) => {
-  let img = [];
-  for (let i = 0; i < data.length; i++) {
-    img.push(data[i].img);
-  }
-  return img;
-}
-const dataName = (data) => {
-  let name = [];
-  for (let i = 0; i < data.length; i++) {
-    name.push(data[i].name);
-  }
-  return name;
-}
-const dataSplash = (data) => {
-  let splash = [];
-  for (let i = 0; i < data.length; i++) {
-    splash.push(data[i].splash);
-  }
-  return splash;
-}
-const dataTitle = (data) => {
-  let title = [];
-  for (let i = 0; i < data.length; i++) {
-    title.push(data[i].title);
-  }
-  return title;
-}
-const dataBlurb= (data)=> {
-  let blurb= [];
-  for (let i = 0;i < data.length; i++) {
-    blurb.push(data[i].blurb);
-  }
-  return blurb;
+const dataLol = (data) => {
+  let dataLol = [];
+  for (let i = 0; i < data.length; i++) {dataLol.push(data[i]);}
+  return dataLol;
 }
 const filterData = (data,condition) => {
-  const filter = data.filter(function(el){
-    return el.tags[0] === condition || el.tags[1] === condition;
-  });
+  const filter = data.filter(function(el){return el.tags[0] === condition || el.tags[1] === condition;});
   return filter;
+}
+const sortData = (data,sortOrder) => {
+  if (sortOrder === 'Asc'){
+    return data.sort();
+  }else{
+    return data.reverse();
+  }
+}
+const computeStats = (data) => {
+  /*la función compute o calcular, nos permitirá hacer cálculos estadísticos básicos para ser
+  mostrados de acuerdo a la data proporcionada. */
+}
+const dataSearch = (data,condition) => {
+  let search=data.filter(function(el){return el.name.toUpperCase().indexOf(condition.toUpperCase())>= 0;});
+  return search;
 }
 const dataInfo = (data) => {
   let info = [];
@@ -47,124 +30,10 @@ const dataInfo = (data) => {
   }
   return info;
 }
-const dataInfoAttack = (data) => {
-  let attack = [];
-  for (let i = 0; i < data.length; i++) {
-    attack.push(data[i].attack);
-  }
-  return attack;
-}
-const dataInfoDefense = (data) => {
-  let defense = [];
-  for (let i = 0; i < data.length; i++) {
-    defense.push(data[i].defense);
-  }
-  return defense;
-}
-const dataInfoMagic = (data) => {
-  let magic = [];
-  for (let i = 0; i < data.length; i++) {
-    magic.push(data[i].magic);
-  }
-  return magic;
-}
-const dataInfoDifficulty = (data) => {
-  let difficulty = [];
-  for (let i = 0; i < data.length; i++) {
-    difficulty.push(data[i].difficulty);
-  }
-  return difficulty;
-}
-//
-const dataStats = (data) => {
-  let stats= [];
-  for (let i= 0; i<data.length; i++) {
-    stats.push(data[i].stats);
-  }
-  return stats;
-}
-const dataStatsHp = (data) => {
-  let hp = [];
-  for (let i = 0; i < data.length; i++) {
-    hp.push(data[i].hp);
-  }
-  return hp;
-}
-const dataStatMp = (data) => {
-  let mp = [];
-  for (let i = 0; i < data.length; i++) {
-    mp.push(data[i].mp);
-  }
-  return mp;
-}
-const dataStatMoveSpeed = (data) => {
-  let movespeed = [];
-  for (let i = 0; i < data.length; i++) {
-    movespeed.push(data[i].movespeed);
-  }
-  return movespeed;
-}
-const dataStatArmor = (data) => {
-  let armor = [];
-  for (let i = 0; i < data.length; i++) {
-    armor.push(data[i].armor);
-  }
-  return armor;
-}
-const dataStatAttackRange = (data) => {
-  let attackrange = [];
-  for (let i = 0; i < data.length; i++) {
-    attackrange.push(data[i].attackrange);
-  }
-  return attackrange;
-}
-const dataStatHpRegen = (data) => {
-  let hpregen = [];
-  for (let i = 0; i < data.length; i++) {
-    hpregen.push(data[i].hpregen);
-  }
-  return hpregen;
-}
-const dataStatMpRegen = (data) => {
-  let mpregen = [];
-  for (let i = 0; i < data.length; i++) {
-    mpregen.push(data[i].mpregen);
-  }
-  return mpregen;
-}
-const dataStatTags = (data) => {
-  let tags = [];
-  for (let i = 0; i < data.length; i++) {
-    tags.push(data[i].tags);
-  }
-  return tags;
-}
-const filterSearch = (data,condition) => {
-  let search= window.data.filter(function(el){
-    return el.name.toUpperCase().indexOf(condition.toUpperCase())>= 0;
-  });
-  return search;
-}
 
 window.data= data;
-window.dataImg = dataImg;
-window.dataName = dataName;//
-window.dataSplash = dataSplash;//
-window.dataTitle = dataTitle;//
+window.dataLol = dataLol;
 window.filterData = filterData
-window.dataInfo = dataInfo;//
-window.dataInfoAttack = dataInfoAttack;
-window.dataInfoDefense = dataInfoDefense;
-window.dataInfoMagic = dataInfoMagic;
-window.dataInfoDifficulty = dataInfoDifficulty;
-window.dataBlurb = dataBlurb;//
-window.dataStats = dataStats;
-window.dataStatHp = dataStatsHp;
-window.dataStatMp = dataStatMp;
-window.dataStatMoveSpeed = dataStatMoveSpeed;
-window.dataStatArmor = dataStatArmor;
-window.dataStatAttackRange = dataStatAttackRange;
-window.dataStatHpRegen = dataStatHpRegen;
-window.dataStatMpRegen = dataStatMpRegen;
-window.dataSearch = filterSearch;//
-window.dataStatTags = dataStatTags;
+window.dataSearch = dataSearch;
+window.sortData = sortData;
+window.dataInfo = dataInfo;
